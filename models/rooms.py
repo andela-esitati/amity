bde5fedb64636d0dd5a65a8acd23bb41b67e8a63
andela-esitati <elsis.sitati@andela.com>
2016-10-15 23:51:30 +0300

    create room model

---
+++ models/rooms.py
@@ -0,0 +1,35 @@
+class Room(object):
+    '''
+    Creates a Person object. Classes Fellow and Staff inherit from it.
+    '''
+
+    def __init__(self, name):
+        self.occupants = []
+        self.name = name
+
+    def __repr__(self):
+        return '<Room %s >' % self.name
+
+    @property
+    def room_type(self):
+        return self.__class__.__name__
+
+
+class Office(Room):
+
+    capacity = 6
+
+    def __init__(self, name):
+        super(Office, self).__init__(name)
+
+        def __repr__(self):
+            return '<Office %s' % self.name
+
+
+class LivingSpace(Room):
+
+    def __init__(self, name):
+        super(LivingSpace, self).__init__(name)
+
+    def __repr__(self):
+        return '<Office %s' % self.name
