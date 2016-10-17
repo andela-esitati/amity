11781d3eea6a76c6eff5f042bf7b4e23d6bb9563
andela-esitati <elsis.sitati@andela.com>
2016-10-17 18:45:20 +0300

    refactor code to remove unused variables

--- models/person.py
+++ models/person.py
@@ -1,1 +1,37 @@
-class Person
+class Person(object):
+    '''This class models a person.Fellow and staff inherit from it '''
+
+    office = None
+
+    def __init__(self, name):
+        self.name = name
+        self.employer_id = id(self)
+
+    def __repr(self):
+        return '<Person %s>' % self.name
+
+
+class Staff(Person):
+
+    role = 'Staff'
+
+    def __init__(self, name):
+        super(Staff, self).__init__(name)
+        self.employer_id = id(self)
+
+    def __repr__(self):
+        return '<Staff %s>' % self.name
+
+
+class Fellow(Person):
+
+    role = 'Fellow'
+    hostel = 'None'
+
+    def __init__(self, name, wants_accomodation='N'):
+        super(Fellow, self).__init__(name)
+        self.employer_id = id(self)
+        self.wants_accomodation = wants_accomodation
+
+    def __repr__(self):
+        return '<Fellow %s>' % self.name
