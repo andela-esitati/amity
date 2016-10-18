c4706541d8b628286a4f005ef6ffe1e76509ef11
andela-esitati <elsis.sitati@andela.com>
2016-10-19 01:38:38 +0300

    implement reallocate person functionality

--- models/amity.py
+++ models/amity.py
@@ -21,7 +21,6 @@ class Amity(object):
         self.allocated_fellows = []
         self.unallocated_fellows = []
 
-
     def create_room(self, name, type_room):
         '''this method create rooms.its can create multiple rooms'''
         if type_room == 'livingspace':
@@ -71,4 +70,46 @@ class Amity(object):
             self.staff.append(person)
             # add staff to all people
             self.all_people.append(person)
-       
+
+    def reallocate_person(self, name, new_room):
+        # go through the list of all people
+        for people in self.all_people:
+            # checking if that person exists
+            if name == people.name:
+                person_found = people
+                break
+
+        # go through a list of all_rooms
+        for room in self.all_rooms:
+            # check that new room is already there
+            if new_room == room.name:
+                # check wether room is an office
+                if isinstance(room, Office):
+                    # check that room is not full
+                    if room.is_not_full:
+                        # add person to members of a room
+                        room.members.append(person_found)
+                        # remove the person from the previous office
+                        person_found.office.members.remove(person_found)
+                        # renaming person office
+                        person_found.office = room
+                # check wether  room is a living space
+                if isinstance(room, LivingSpace):
+                    # check that room is not full
+                    if room.is_not_full:
+                        room.members.append(person_found)
+                        person_found.hostel.members.remove(person_found)
+                        person_found.hostel = person_found
+
+
+amity = Amity()
+amity.create_room('valhalla', 'office')
+amity.create_room('oculus', 'office')
+amity.create_room('haskel', 'livingspace')
+print amity.all_rooms
+amity.add_person('stan', 'fellow', 'Y')
+amity.add_person('elsis', 'staff')
+v = amity.all_rooms[1]
+print v.members
+amity.reallocate_person('elsis','valhalla')
+print v.members
