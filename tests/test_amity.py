a58d20b602e06b583db297d55ea6c9e7ca50e8ea
andela-esitati <elsis.sitati@andela.com>
2016-10-20 17:55:12 +0300

    test to confirm office is created

--- tests/test_amity.py
+++ tests/test_amity.py
@@ -2,12 +2,26 @@ import unittest
 from models.amity import Amity
 
 
-class TestAmity(unittest.TestCase): 
+class TestAmity(unittest.TestCase):
     def setUp(self):
         self.amity = Amity()
 
     def test_create_room(self):
+        '''test to confirm a room has been created'''
         rooms = self.amity.all_rooms
+        # making sure the rooms are empty
         self.assertEqual(len(rooms), 0)
-        self.amity.create_room('valhalla', 'office')
+        self.amity.create_room('haskel', 'livingspace')
+        # checking to see if rooms have been added
         self.assertEqual(len(rooms), 1)
+    
+    def test_office_is_created(self):
+        '''test to confirm an office is created'''
+        office = self.amity.office_rooms
+        self.assertEqual(len(office), 0)
+        self.amity.create_room('valhalla', 'office')
+        self.assertEqual(len(office), 1)
+
+    
+
+
