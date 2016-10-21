289ef4c879bfc535bf18d681c61cf73e32faae36
andela-esitati <elsis.sitati@andela.com>
2016-10-21 11:04:20 +0300

    change class variable to instance variable

--- models/person.py
+++ models/person.py
@@ -1,11 +1,10 @@
 class Person(object):
     '''This class models a person.Fellow and staff inherit from it '''
 
-    office = None
-
     def __init__(self, name):
         self.name = name
         self.employer_id = id(self)
+        self.office = None
 
     def __repr(self):
         return '<Person %s>' % self.name
@@ -23,12 +22,11 @@ class Staff(Person):
 
 class Fellow(Person):
 
-    hostel = 'None'
-
     def __init__(self, name, wants_accomodation='N'):
         super(Fellow, self).__init__(name)
         self.employer_id = id(self)
         self.wants_accomodation = wants_accomodation
+        self.hostel = 'None'
 
     def __repr__(self):
         return '<Fellow %s>' % self.name
